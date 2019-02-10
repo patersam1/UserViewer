e48b9af76c81c01a52c13db30d3e7691eb2a9566
Sam Paterson <>
2019-02-10 17:38:14 +1300

    Added all VC and swift files with basic setup

---
+++ UserViewer/Scripts/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  UserViewer
+//
+//  Created by Sam Paterson on 10/02/19.
+//  Copyright © 2019 Sam Paterson. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
