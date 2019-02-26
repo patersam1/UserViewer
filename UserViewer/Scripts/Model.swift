2eba63b9982c47dc770d1555e288658046a3ddc8
Sam Paterson <>
2019-02-26 13:26:44 +1300

    cleaned up JSON stuff

--- UserViewer/Scripts/Model.swift
+++ UserViewer/Scripts/Model.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-//struct Users: Decodable{
-//    let user: [String : User]
-//}
 struct User: Codable{
     let firstName: String
     let lastName: String
@@ -21,7 +18,6 @@ struct User: Codable{
 class Model{
     static let instance = Model()
     
-//    var userDic: [String : [String]] = [:]
     var users: [String: User] = [:]
     
     func addUser(fName: String, lName: String, age: String, email: String)-> Bool{
@@ -42,86 +38,31 @@ class Model{
             encoder.outputFormatting = .prettyPrinted
             let usersArray = Array(users.values)
             let data = try encoder.encode(usersArray)
-            print(String(data: data, encoding: .utf8)!)
             
             //writing to file
-//            guard let path = Bundle.main.path(forResource: "TestUsers", ofType: "json") else { print("path write error"); return -1}
             guard let docUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {return -1}
             let fileUrl = docUrl.appendingPathComponent("Users.json")
-//            print("path - \(path)")
             try data.write(to: fileUrl)
         } catch{
             print(error)
         }
-        
-        
-        
-//        var topLevelDict: [String: Any] = [:]
-//        for (key, value) in userDic{
-//            var user: [String : String] = [:]
-//            user["fName"] = value[0]
-//            user["lName"] = value[1]
-//            user["age"] =  value[2]
-//            user["email"] = value[3]
-//            topLevelDict[key] = user
-//
-//        }
-////        print(topLevelDict)
-//        //creates the json data
-//        guard let jsonData = try? JSONSerialization.data(withJSONObject: topLevelDict, options: .prettyPrinted) else{
-//            return -1
-//        }
-//
-//
-//        guard let docUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {return -1}
-//        let fileUrl = docUrl.appendingPathComponent("Users.json")
-//        print("Write url \(fileUrl)")
-//        do{
-//            try jsonData.write(to: fileUrl, options: [])
-//        }catch{
-//            print("error writing data to disk \(error)")
-//            return -1
-//        }
-        print("write success")
         return 0
     }
     
     func loadUsersFromJSON(){
         
-//        guard let path = Bundle.main.path(forResource: "TestUsers", ofType: "json") else { print("error with path"); return }
         guard let docUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {return}
         let fileUrl = docUrl.appendingPathComponent("Users.json")
-//        print("path - \(path)")
         
         do{
             let data = try Data(contentsOf: fileUrl)
-            let tempUsers = try JSONDecoder().decode([User].self, from: data)
-            for user in tempUsers{
+            let usersArray = try JSONDecoder().decode([User].self, from: data)
+            for user in usersArray{
                 users[user.firstName+user.lastName] = user
             }
         }catch{
             print(error)
         }
-        
-        
-//        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else { print("error with json"); return }
-//
-//        if let jsonDic = json as? [String: [String: String]]{
-//            for (key, value) in jsonDic{
-//
-//                guard let fName = value["fName"],
-//                    let lName = value["fName"],
-//                    let age = value["age"],
-//                    let email = value["email"]
-//                else {
-//                    print("error extecting data")
-//                    continue
-//                }
-//
-//                var userArray = [fName, lName, age, email]
-//                userDic[key] = userArray
-//            }
-//        }
         
     }
     
